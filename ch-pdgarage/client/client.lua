8dc4584cd12cf99455cfc1db72c8cb0f953b8eee
Protocol <155591848+protocold3v@users.noreply.github.com>
2024-06-21 17:16:30 -0400

    Update client.lua

--- ch-pdgarage/client/client.lua
+++ ch-pdgarage/client/client.lua
@@ -1,3 +1,5 @@
+local carOut = false
+
 AddEventHandler('onResourceStart', function(resource)
     if resource == GetCurrentResourceName() then
          TargetPdGarag()
@@ -41,25 +43,34 @@ for k,v in ipairs(Chapo.GarageMenu) do
     table.insert(garagemenu, {
         title = v.label,
         onSelect = function()
-            if not IsModelInCdimage(v.name) then return end
-            RequestModel(v.name) 
-            while not HasModelLoaded(v.name) do 
-              Wait(0)
-            end
-            currentCar = CreateVehicle(v.name, Chapo.VehSpawnCoords, false, false) 
-            SetVehicleNumberPlateText(currentCar, v.platename)
-            SetPedIntoVehicle(PlayerPedId(),currentCar,-1)  
-            lib.notify({
-                title = 'Véhicule Sortie !',
-                description = 'Votre véhicule de service est sortie',
-                type = 'success'
-            })
-            if Chapo.Framework == 'qb' then    
-                TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(currentCar)) 
-            end
-            if Chapo.CleVehicule then
-                local plate = GetVehicleNumberPlateText(currentCar)
-                keyusingget(plate)
+            if not carOut then
+                if not IsModelInCdimage(v.name) then return end
+                RequestModel(v.name) 
+                while not HasModelLoaded(v.name) do 
+                  Wait(0)
+                end
+                currentCar = CreateVehicle(v.name, Chapo.VehSpawnCoords, false, false) 
+                SetVehicleNumberPlateText(currentCar, v.platename)
+                SetPedIntoVehicle(PlayerPedId(),currentCar,-1)  
+                carOut = true
+                lib.notify({
+                    title = 'Véhicule Sortie !',
+                    description = 'Votre véhicule de service est sortie',
+                    type = 'success'
+                })
+                if Chapo.Framework == 'qb' then    
+                    TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(currentCar)) 
+                end
+                if Chapo.CleVehicule then
+                    local plate = GetVehicleNumberPlateText(currentCar)
+                    keyusingget(plate)
+                end
+            else
+                lib.notify({
+                    title = 'Véhicule déja sortie !',
+                    description = 'Un véhicule est déja sortie ranger le avant de sortire un autre !',
+                    type = 'error'
+                })
             end
         end
     })
@@ -71,12 +82,14 @@ lib.registerContext({
     options = {
         {
             title = 'Sortir un véhicule',
+            icon = 'right-from-bracket',
             onSelect = function()
                 lib.showContext('chpd_menu_garade_car')
             end
         },
         {
             title = 'Ranger un véhicule',
+            icon = 'square-parking',
             onSelect = function()
                 DeleteVehicle(currentCar)
                 lib.notify({
@@ -84,7 +97,22 @@ lib.registerContext({
                     description = 'Votre véhicule de service est ranger',
                     type = 'success'
                 })
+                carOut = false
             end
+        },
+        {
+            title = 'Signaler disparaition',
+            description = 'signaler un véhicule disparu !',
+            icon = 'bell',
+            onSelect = function()
+                lib.notify({
+                    title = 'Véhicule signalé !',
+                    description = 'Vous avez signaler la dispairation de votre véhicule !',
+                    type = 'success'
+                })
+                DeleteVehicle(currentCar)
+                carOut = false
+            end,
         },
     }
 }) 
