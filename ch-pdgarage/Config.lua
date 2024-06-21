1a3ff8b827c989d615261dd9ecc213887b7c1f80
Protocol <155591848+protocold3v@users.noreply.github.com>
2024-06-21 17:19:20 -0400

    Update Config.lua

--- ch-pdgarage/Config.lua
+++ ch-pdgarage/Config.lua
@@ -1,11 +1,11 @@
-Chapo = {}
+Ptc = {}
 
-Chapo.Framework = 'qb' --- esx or qb
-Chapo.ProgressBar = true -- true or false (progressbar quand tu sort le vehicule)
-Chapo.ProgressType = 'bar' -- bar or circle
-Chapo.CleVehicule = false -- false or true
+Ptc.Framework = 'qb' --- esx or qb
+Ptc.ProgressBar = true -- true or false (progressbar quand tu sort le vehicule)
+Ptc.ProgressType = 'bar' -- bar or circle
+Ptc.CleVehicule = false -- false or true
 
-Chapo.VehSpawnCoords = vector4(449.1787, -981.3781, 25.6998, 72.6402) --- position spawn vehicule
+Ptc.VehSpawnCoords = vector4(449.1787, -981.3781, 25.6998, 72.6402) --- position spawn vehicule
 
 function keyusingget(plate)
     exports['mono_garage']:ClientInventoryKeys(plate, 'add')
@@ -15,13 +15,13 @@ function clevehremove(plate)
     exports['mono_garage']:ClientInventoryKeys(plate, 'remove')
 end
 
-Chapo.GarageMenu = { 
+Ptc.GarageMenu = { 
     {name = 'police', label = 'Police', platename = 'CHAPO'}, 
     {name = 'police2', label = 'Police 2', platename = 'POLICE'},
     {name = 'sultan', label = 'Sultan', platename = 'POLICE'},
 }
 
-Chapo.ConfigPdGarage = {
+Ptc.ConfigPdGarage = {
     [1] = { 
         TargetZone = {
 			ped = {
